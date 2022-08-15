bb6aa2156aaeb625a859a85529fae3a49fcdc3d4
chostler24 <connor.hostler@gmail.com>
2022-08-15 13:09:01 -0700

    Adding task 0 and header file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - sorts an array of integers in ascending order
+  * using the bubble sort algorithm.
+  * @array: array of integers
+  * @size: size of array
+  * Return: none
+  */
+
+void bubble_sort(int *array, size_t size)
+{
+}
