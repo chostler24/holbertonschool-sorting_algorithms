30f3a956458793f1d9e13ef8cdfa22222e7ceb06
chostler24 <connor.hostler@gmail.com>
2022-08-16 12:01:32 -0700

    Updating header file

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void node_swap(listint_t **head, listint_t **list);
 
 #endif
