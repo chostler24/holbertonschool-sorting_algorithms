ccd20835447e40576377479721740ec05d2314c8
chostler24 <connor.hostler@gmail.com>
2022-08-17 12:42:14 -0700

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+  * quick_sort - sorts an array of integers in ascending order
+  * using the quick sort algorithm
+  * @array: array of integers
+  * @size: size of array
+  * Return: none
+  */
+
+void quick_sort(int *array, size_t size)
+{
+}
